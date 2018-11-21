6f1ece8e0e0b0eb0df7e0ead3664396e4e111686
宛天 <zhaobin.zb@alibaba-inc.com>
2018-11-21 16:58:54 +0800

    bugfix

--- demo/index.js
+++ demo/index.js
@@ -1,31 +1,42 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 import DataSheet from '../src/index';
 import '../src/react-datasheet.css';
-import SelectEditor from '../src/DataSelect'
-import filedata from './mock'
+import SelectEditor from '../src/DataSelect';
+import filedata from './mock';
 
 const data = [
-  [{ name: '',value:"a"}, { name: '',value:"b"},  { name: '', value:"c"},{ name: '', value:"c" }, { name: '',value:"d", dataEditor: SelectEditor }], 
+  [
+    {name: '', value: 'a'},
+    {name: '', value: 'b'},
+    {name: '', value: 'c'},
+    {name: '', value: 'c'},
+    {name: '', value: 'd', dataEditor: SelectEditor},
+  ],
 ];
-const adddata = 
-  [{ name: '',value:"1"}, { name: '',value:"2"},   { name: '', value:"3" }, { name: '', value:"4"},{ name: '', value:"5",dataEditor: SelectEditor }];
+const adddata = [
+  {name: '', value: '1'},
+  {name: '', value: '2'},
+  {name: '', value: '3'},
+  {name: '', value: '4'},
+  {name: '', value: '5', dataEditor: SelectEditor},
+];
 
 const columns = [
-     { label: 'col1', width: '25%' },
-   { label: 'col2', width: '25%' },
-   { label: 'col3 (SRM)', width: '25%' },
-    { label: 'col4', width: '25%' },
-    { label: 'col5', width: '25%' }
- ];
+  {label: 'col1', width: '25%'},
+  {label: 'col2', width: '25%'},
+  {label: 'col3 (SRM)', width: '25%'},
+  {label: 'col4', width: '25%'},
+  {label: 'col5', width: '25%'},
+];
 const App = () => {
   return (
     <div>
-      <DataSheet adddata = {adddata} columns = {columns}  data = {data} />
+      <DataSheet adddata={adddata} columns={columns} data={data} />
     </div>
   );
 };
 
 // export default App;
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render (<App />, document.getElementById ('app'));
--- demo/mock.js
+++ demo/mock.js
@@ -1,61 +1,61 @@
- const fileData = {
-    "templateId": 1,
-    "dictList": [
+const fileData = {
+  templateId: 1,
+  dictList: [
+    {
+      dictId: '', //为空
+      entityId: 'entity1',
+      nodeId: 'nodeId',
+      bizType: 'EQUIPMENT',
+      fieldList: [
         {
-            "dictId": "", //为空
-            "entityId": "entity1",
-            "nodeId": "nodeId",
-            "bizType": "EQUIPMENT",
-            "fieldList": [
-                {
-                    "fieldId": "", //初始化只新建一条
-                    "fieldData": [
-                        {
-                            "name": "attribute",
-                            "value": ""
-                        },
-                        {
-                            "name": "unit",
-                            "value": ""
-                        },
-                        {
-                            "name": "filterRule",
-                            "value": ""
-                        },
-                        {
-                            "name": "fieldType",
-                            "value": ""
-                        },
-                        {
-                            "name": "sourceField",
-                            "value": ""
-                        }
-                    ],
-                    "fieldData": [
-                        {
-                            "name": "attribute",
-                            "value": ""
-                        },
-                        {
-                            "name": "unit",
-                            "value": ""
-                        },
-                        {
-                            "name": "filterRule",
-                            "value": ""
-                        },
-                        {
-                            "name": "fieldType",
-                            "value": ""
-                        },
-                        {
-                            "name": "sourceField",
-                            "value": ""
-                        }
-                    ]
-                }
-            ]
-        }
-    ]
-  }
-  export {fileData}
+          fieldId: '', //初始化只新建一条
+          fieldData: [
+            {
+              name: 'attribute',
+              value: '',
+            },
+            {
+              name: 'unit',
+              value: '',
+            },
+            {
+              name: 'filterRule',
+              value: '',
+            },
+            {
+              name: 'fieldType',
+              value: '',
+            },
+            {
+              name: 'sourceField',
+              value: '',
+            },
+          ],
+          fieldData: [
+            {
+              name: 'attribute',
+              value: '',
+            },
+            {
+              name: 'unit',
+              value: '',
+            },
+            {
+              name: 'filterRule',
+              value: '',
+            },
+            {
+              name: 'fieldType',
+              value: '',
+            },
+            {
+              name: 'sourceField',
+              value: '',
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+export {fileData};
